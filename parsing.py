06fe86a635a1016e55bdd4e7b1be78aab7e356b2
Tim O'Brien <timothy.c.obrien99@gmail.com>
2020-09-03 14:20:04 -0500

    cleanup

--- parsing.py
+++ parsing.py
@@ -5,7 +5,9 @@ conf.setAppName("big-data-workshop").setMaster("local")
 sc: pyspark.SparkContext = pyspark.context.SparkContext.getOrCreate(conf=conf)
 
 no_e = sc.textFile("no-e.txt")
+# gets words from text
 word_e = no_e.flatMap(lambda line: line.split(" "))
+# gets characters from words
 char_e = word_e.flatMap(lambda word: list(word))
 
 freq_e = char_e.countByValue()
@@ -30,8 +32,18 @@ print(word_pinoc.reduce(lambda w1, w2: w1 if len(w1) > len(w2) else w2))
 word_unique_pinoc = word_pinoc.distinct()
 
 # words that start with s
-word_s_pinoc.filter(lambda w: w.startswith("s"))
+word_s_pinoc = word_pinoc.filter(lambda w: w.startswith("s"))
 
 char_pinoc = word_e.flatMap(lambda word: list(word))
 total_char = char_pinoc.count()
 freq_pinoc = pinoc.countByValue()
+
+books = sc.textFile("books/*.txt")
+books_w = books.flatMap(lambda line: line.split(" "))
+# converts words to uppercase before splitting
+books_c = books_w.flatMap(lambda word: list(word.upper()))
+# filtering out anything that isn't a letter
+books_c = books_c.filter(lambda char: 'A' <= char <= 'Z')
+
+for c in range(ord('a'), ord('z') + 1):
+    # need to finish
